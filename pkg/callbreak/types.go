8d7e57329d7e4f66b767f9a7775b537a7168c28b
prasant adhikari <pa1038@nyu.edu>
2023-06-28 16:31:05 -0400

    hand is now a type provided by game

--- pkg/callbreak/types.go
+++ pkg/callbreak/types.go
@@ -1,7 +1,11 @@
 package callbreak
 
+import "github.com/prasantadh/callbreak-go/pkg/deck"
+
 const (
 	NCards   = 52
 	NPlayers = 4
 	NTricks  = NCards / NPlayers
 )
+
+type Hand [NTricks]deck.Card
