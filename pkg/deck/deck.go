17aa38b099a3d44c3b6df59aa2c5630686e7429f
prasant adhikari <pa1038@nyu.edu>
2023-06-28 19:24:34 -0400

    cards added to deck are Playable by default

--- pkg/deck/deck.go
+++ pkg/deck/deck.go
@@ -8,7 +8,7 @@ func New() Deck {
 	d := Deck{}
 	for _, r := range Ranks {
 		for _, s := range Suits {
-			d.cards = append(d.cards, Card{Rank: r, Suit: s})
+			d.cards = append(d.cards, Card{Rank: r, Suit: s, Playable: true})
 		}
 	}
 	return d
