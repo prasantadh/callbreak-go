9b6edfb34a67d71e9da57d3bbf6ccc849fd5fd56
prasant adhikari <pa1038@nyu.edu>
2023-06-29 16:04:09 -0400

    update bot play logic

--- pkg/bot/bot.go
+++ pkg/bot/bot.go
@@ -1,6 +1,8 @@
 package bot
 
 import (
+	"fmt"
+
 	"github.com/prasantadh/callbreak-go/pkg/callbreak"
 	"github.com/prasantadh/callbreak-go/pkg/deck"
 )
@@ -16,51 +18,62 @@ func New(n string) *Player {
 	}
 }
 
-func (p *Player) Play(t callbreak.Trick) deck.Card {
+func (p *Player) Play(t callbreak.Trick) (deck.Card, error) {
 
-	for i, c := range p.Hand {
-		if c.Playable {
-			p.Hand[i].Playable = false
-			return c
+	// // play the first playable card
+	// for i, c := range p.Hand {
+	// 	if c.Playable {
+	// 		p.Hand[i].Playable = false
+	// 		return c
+	// 	}
+	// }
+
+	// for an empty trick play the first playable card on hand
+	if t.Size == 0 {
+		for i, c := range p.Hand {
+			if c.Playable {
+				p.Hand[i].Playable = false
+				return c, nil
+			}
+		}
+		msg := fmt.Errorf("No playable card left!")
+		return deck.Card{}, msg
+	}
+
+	// if we have a card of the leading suit, play it
+	for _, card := range p.Hand {
+		if card.Suit == t.Lead.Suit &&
+			card.Playable {
+			// TODO: sort the hand by rank so that we play the highest
+			// by default. IMP because if there is a winning card,
+			// we must play the winning card
+			return card, nil
 		}
 	}
 
-	// // for an empty trick, play the first playable card on hand
-	// hand := p.Hand
-	// if t.Size == 0 {
-	// 	i := 0
-	// 	for ; i < callbreak.NTricks && !hand[i].Playable; i++ {
-	// 		fmt.Println(hand[i])
-	// 	}
-	// 	return hand[i]
-	// }
+	// if we have a Hukum card, play it
+	for _, card := range p.Hand {
+		// TODO: eventually check to see if the hukum we are playing
+		// wins all cards in the trick, else play non-Hukum
+		// for now, playing the largest hukum is a valid move.
+		if card.Suit == deck.Hukum {
+			return card, nil
+		}
+	}
 
-	// // if we have a card of the same suit, play it
-	// for _, card := range p.Hand {
-	// 	if card.Suit == t.Lead.Suit &&
-	// 		card.Playable {
-	// 		// TODO: sort the hand by rank so that we play the highest
-	// 		// by default. IMP because if there is a winning card,
-	// 		// we must play the winning card
-	// 		return card
-	// 	}
-	// }
-
-	// // if we have a Hukum card, play it
-	// for _, card := range p.Hand {
-	// 	// TODO: eventually check to see if the hukum we are playing
-	// 	// wins all cards in the trick, else play non-Hukum
-	// 	// for now, playing the largest hukum is a valid move.
-	// 	if card.Suit == deck.Hukum {
-	// 		return card
-	// 	}
-	// }
-
-	return deck.Card{}
+	// we don't have the leading suit and we don't have Hukum
+	// play any random playable card
+	var ans deck.Card
+	for i, c := range p.Hand {
+		if c.Playable {
+			p.Hand[i].Playable = false
+			ans = c
+		}
+	}
+	return ans, nil
 }
 
 func (p *Player) Take(c deck.Card) error {
 	p.Hand = append(p.Hand, c)
-	// fmt.Println(p.Name, p.Hand)
 	return nil
 }
