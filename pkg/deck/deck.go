ba3ffe0bbbec398f783c27ecf88dd94f6320ee97
prasant adhikari <pa1038@nyu.edu>
2023-06-28 21:14:07 -0400

    deck.Deck is now a slice, exports Size

--- pkg/deck/deck.go
+++ pkg/deck/deck.go
@@ -1,14 +1,16 @@
 package deck
 
-type Deck struct {
-	cards []Card
-}
+const (
+	Size = 52
+)
+
+type Deck []Card
 
 func New() Deck {
 	d := Deck{}
 	for _, r := range Ranks {
 		for _, s := range Suits {
-			d.cards = append(d.cards, Card{Rank: r, Suit: s, Playable: true})
+			d = append(d, Card{Rank: r, Suit: s, Playable: true})
 		}
 	}
 	return d
