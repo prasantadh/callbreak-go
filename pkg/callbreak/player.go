461272df98112432a63ca4227eb9e09cd82d862a
Prasant Adhikari <pa1038@nyu.edu>
2023-07-08 15:42:18 -0400

    [player] provide Add() method

--- pkg/callbreak/player.go
+++ pkg/callbreak/player.go
@@ -1,7 +1,21 @@
 package callbreak
 
-import "github.com/prasantadh/callbreak-go/pkg/deck"
+import (
+	"fmt"
+
+	"github.com/prasantadh/callbreak-go/pkg/deck"
+)
 
 func (p *player) Take(c deck.Card) {
 	p.hand = append(p.hand, c)
+}
+
+func (p *player) Play(c deck.Card) error {
+	for i, card := range p.hand {
+		if card.Suit == c.Suit && card.Rank == c.Rank {
+			p.hand[i].Playable = false
+			return nil
+		}
+	}
+	return fmt.Errorf("card not in player's hand")
 }
