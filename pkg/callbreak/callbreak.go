77dc74753b4ea83d8774424f808b2a7ba6d11040
Prasant Adhikari <pa1038@nyu.edu>
2023-07-08 15:47:03 -0400

    [callbreak] use player.Play() and trick.Add(). provide Update()

--- pkg/callbreak/callbreak.go
+++ pkg/callbreak/callbreak.go
@@ -7,13 +7,16 @@ import (
 )
 
 type CallBreak struct { // a game is
-	players      []player // between 4 players
-	deck.Deck             // with a standard deck of cards
-	CurrentTrick Trick
+	players    []player // between 4 players
+	deck.Deck           // with a standard deck of cards
+	tricks     []Trick  // and is played as a collection of Tricks
+	NextPlayer int
 }
 
 func New() *CallBreak {
-	return &CallBreak{}
+	return &CallBreak{
+		tricks: make([]Trick, 1),
+	}
 }
 
 func (g *CallBreak) AddPlayer(p PlayerInterface) error {
@@ -55,10 +58,43 @@ func (g *CallBreak) GetHand(i int) Hand {
 	return g.players[i].hand
 }
 
-// the next player in line playes the card c
-func (g *CallBreak) Play(c deck.Card) error {
-	// update internal state to indicate that
-	// a player has played a card in their turn
-	return nil
+// necessary update at the end of a trick
+func (g *CallBreak) Update() {
+	trick := &g.tricks[len(g.tricks)-1]
+	if trick.Size == NPlayers {
+		if len(g.tricks) < NTricks {
+			g.NextPlayer = trick.Winner()
+			g.tricks = append(g.tricks, Trick{Lead: g.NextPlayer})
+		}
+	}
+}
 
+// the next player in line playes the card c
+// TODO: authorize the player for this action
+func (g *CallBreak) Play(c deck.Card) error {
+
+	g.Update() // Critical to call this
+
+	// the player plays the card
+	player := &g.players[g.NextPlayer]
+	err := player.Play(c)
+	if err != nil {
+		return fmt.Errorf("game could not play: %v", err)
+	}
+	g.NextPlayer = (g.NextPlayer + 1) % NPlayers
+
+	// the card gets added to the trick
+	// TODO: it's a problem if player.Play() succeeds
+	// but trick.Add fails
+	trick := &g.tricks[len(g.tricks)-1]
+	err = trick.Add(c)
+	if err != nil {
+		return fmt.Errorf("game could not add to trick: %v", err)
+	}
+
+	return nil
+}
+
+func (g *CallBreak) CurrentTrick() Trick {
+	return g.tricks[len(g.tricks)-1]
 }
