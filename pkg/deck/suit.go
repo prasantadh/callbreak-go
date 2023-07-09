2a50070c3774f1dd006cf17dbd06253408abd6a0
Prasant Adhikari <pa1038@nyu.edu>
2023-07-09 11:49:20 -0400

    [suit] use filled in unicode for values

--- pkg/deck/suit.go
+++ pkg/deck/suit.go
@@ -3,10 +3,10 @@ package deck
 type Suit rune
 
 const (
-	Hukum Suit = '♤'
-	Chidi      = '♧'
-	Itta       = '♢'
-	Paan       = '♡'
+	Hukum Suit = '♠'
+	Chidi      = '♣'
+	Itta       = '♦'
+	Paan       = '♥'
 )
 
 var Suits [4]Suit = [...]Suit{Hukum, Chidi, Itta, Paan}
