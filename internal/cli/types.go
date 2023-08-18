68231d5025400f772ce05cc0ead8901f376ec434
Prasant Adhikari <pa1038@nyu.edu>
2023-08-18 16:13:28 -0400

    type definition for easier data handling

--- internal/cli/types.go
+++ internal/cli/types.go
@@ -1,19 +1,56 @@
 package cli
 
-import "net/url"
+import (
+	"github.com/prasantadh/callbreak-go/pkg/callbreak"
+	"github.com/prasantadh/callbreak-go/pkg/deck"
+)
 
 type Status string
 
-const (
-	Success Status = "success"
-	Failure Status = "failure"
-)
-
 type Response struct {
 	Status `json:"status"`
-	Data   any `json:"data"`
+	Data   string `json:"data"`
 }
 
-type Options struct {
-	Server url.URL
+const (
+	Success Status = "success"
+	Failure        = "failure"
+)
+
+type callRequest struct {
+	Token callbreak.Token `json:"token"`
+	Call  callbreak.Score `json:"call"`
 }
+
+type queryRequest struct {
+	Token callbreak.Token `json:"token"`
+}
+
+type breakRequest struct {
+	Token callbreak.Token `json:"token"`
+	Suit  deck.Suit       `json:"suit"`
+	Rank  deck.Rank       `json:"rank"`
+}
+
+type registerRequest callbreak.PlayerConfig
+
+/*
+request->
+   /new -> {
+       -- "debug" : true,
+       -- [{"name": "bot0", "strategy" : "basic", "timeout": "500"},{},{}]
+   }
+   /query -> {
+       "token" : "deadbeef"
+   }
+   /register -> {
+       {"name" : "bot0", "strategy" :"basic", "timeout" : "500"}
+   }
+   /call -> {
+       {"token" : "deadbeef", "call" : "5"}
+   }
+   /break
+       {"token" : "deadbeef", "suit" : "Hukum", "rank" : "Ekka"}
+response->
+ {"status" : "success/failure", "data" : game}
+*/
